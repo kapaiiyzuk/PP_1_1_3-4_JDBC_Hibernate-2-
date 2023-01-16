b37d945257a4aa27ea97977572d6c3acaddaf55c
kapaiiyzuk <118076489+kapaiiyzuk@users.noreply.github.com>
2023-01-16 17:55:50 +0300

    Update UserServiceImpl.java

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -18,7 +18,6 @@ public class UserServiceImpl implements UserService {
 
     public void saveUser(String name, String lastName, byte age) {
         userDaoHibernate.saveUser(name, lastName, age);
-        System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
